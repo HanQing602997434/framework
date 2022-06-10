9b3ba2d88383877165f013b2dc470d585fc4b620
hq <602997434@qq.com>
2022-06-09 23:22:52 -0700

    perforce

---
+++ p4v.c
@@ -0,0 +1,13 @@
+
+// perforce
+
+/*
+    P4服务器搭建
+    	p4配置: /usr/p4config/p4.configfile
+    	启动服务器: p4d &
+    	查看服务器和客户端信息: p4 info
+    	查看用户信息: p4 users
+    	查看仓库信息: p4 depots
+    	删除仓库: p4 depot -d name
+    	删除用户: p4 user -d name
+*/
